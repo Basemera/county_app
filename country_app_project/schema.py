657d124b60d557fe8694940228d0e0110454bf3d
Ephraim Malinga <ephraim.malinga@gmail.com>
2019-10-29 14:56:18 +0300

    Dev (#6)
    * Add travis integration (#2)
    
    * Add travis integration
    
    * modified .travis.yml file and .gitignore file
    
    * modified .travis.yml file
    
    * Ft deploy heroku 166707671 (#4)
    
    * Add travis integration
    
    * Add test coverage and static code analysis with CodeClimate
    
    * Added Procfile
    
    * modified Procfile
    
    * Modified Procfile
    
    * Ft deploy heroku 166707671 (#5)
    
    * Add travis integration
    
    * Add test coverage and static code analysis with CodeClimate
    
    * Added Procfile
    
    * modified Procfile
    
    * Modified Procfile
    
    * modified .env settings
    
    * fixing errors
    
    * Fix syntax errors
    
    * added gunicorn file
    
    *  added dotenv module
    
    * Add url to allowed hosts
    
    * Fix forward slash error in  allowed hosts
    
    * Add correct domain to Allowed hosts
    
    * modified view files
    
    * modified gitignore file
    
    * Add validation for email and password and register user mutation
    
    * removed .env file from git treacking

--- country_app_project/schema.py
+++ country_app_project/schema.py
@@ -3,12 +3,16 @@ from country_list.schema.query.country import Query as CountryQuery
 from country_list.schema.query.city import Query as CityQuery
 from country_list.schema.query.subcountry import Query as SubCountryQuery
 from country_list.schema.mutations.country import Mutation as CreateCountryMutation
+from auth.schema.mutations.register import Mutation as RegisterUser
+from country_list.schema.mutations.city import Mutation as CreateCityMutation
+
 
 
 class Query(CountryQuery, CityQuery, SubCountryQuery, graphene.ObjectType):
     pass
 
-class Mutation(CreateCountryMutation, graphene.ObjectType):
+class Mutation(CreateCountryMutation, RegisterUser, graphene.ObjectType):
+    class Mutation(CreateCountryMutation, CreateCityMutation, graphene.ObjectType):
     pass
 
 schema = graphene.Schema(query=Query, mutation=Mutation)
